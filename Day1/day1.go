6c8dd123d062d9d6581406c2eb7ad52fa338b7d1
j-talev <j.talev@outlook.com>
2024-05-30 21:41:24 +1000

    improve var definitions

--- Day1/day1.go
+++ Day1/day1.go
@@ -15,31 +15,31 @@ func main() {
 	}
 	defer file.Close()
 
-	s := make([]string, 0)
+	words := make([]string, 0)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		s = append(s, scanner.Text())
+		words = append(words, scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("scanner error: %v", err)
 	}
 
-	vals := make([]int, 0)
+	nums := make([]int, 0)
 
-	for _, str := range s {
+	for _, word := range words {
 		var firstDigit, lastDigit string
 
-		for _, char := range str {
+		for _, char := range word {
 			if _, err := strconv.Atoi(string(char)); err == nil {
 				firstDigit = string(char)
 				break
 			}
 		}
 
-		for i := len(str) - 1; i >= 0; i-- {
-			if _, err := strconv.Atoi(string(str[i])); err == nil {
-				lastDigit = string(str[i])
+		for i := len(word) - 1; i >= 0; i-- {
+			if _, err := strconv.Atoi(string(word[i])); err == nil {
+				lastDigit = string(word[i])
 				break
 			}
 		}
@@ -47,13 +47,13 @@ func main() {
 		num := firstDigit + lastDigit
 		digit, err := strconv.Atoi(string(num)); 
 		if err == nil {
-			vals = append(vals, digit)
+			nums = append(nums, digit)
 		}
 	}
 
 	sum := 0
-	for _, val := range vals {
-		sum += val
+	for _, num := range nums {
+		sum += num
 	}
 	fmt.Println(sum)
 }
