48442e79f78be64acd7bd656fb318a438e7079c0
j-talev <j.talev@outlook.com>
2024-05-30 22:45:26 +1000

    improve algorithms

--- Day1/day1.go
+++ Day1/day1.go
@@ -8,49 +8,66 @@ import (
 	"strconv"
 )
 
-func main() {
-	file, err := os.Open("input.txt")
-	if err != nil {
-		log.Fatalf("failed to open file: %v", err)
-	}
-	defer file.Close()
-
-	words := make([]string, 0)
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		words = append(words, scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatalf("scanner error: %v", err)
-	}
-
+func getFirstLastDigit(words []string) []int {
 	nums := make([]int, 0)
-
+	
 	for _, word := range words {
 		var firstDigit, lastDigit string
-
-		for _, char := range word {
-			if _, err := strconv.Atoi(string(char)); err == nil {
-				firstDigit = string(char)
+		i, j := 0, len(word)-1
+	
+		for i < len(word) {
+			if firstDigit == "" {
+				if _, err := strconv.Atoi(string(word[i])); err == nil {
+					firstDigit = string(word[i])
+				}
+			}
+			if lastDigit == "" {
+				if _, err := strconv.Atoi(string(word[j])); err == nil {
+					lastDigit = string(word[j])
+				}
+			}
+			if firstDigit != "" && lastDigit != "" {
 				break
 			}
+			i++
+			j--
 		}
-
-		for i := len(word) - 1; i >= 0; i-- {
-			if _, err := strconv.Atoi(string(word[i])); err == nil {
-				lastDigit = string(word[i])
-				break
-			}
-		}
-
+	
 		num := firstDigit + lastDigit
-		digit, err := strconv.Atoi(string(num)); 
+		digit, err := strconv.Atoi(num)
 		if err == nil {
 			nums = append(nums, digit)
 		}
 	}
 
+	return nums
+}
+
+func getWordsSlice(doc string) []string {
+	words := make([]string, 0)
+
+	file, err := os.Open(doc)
+	if err != nil {
+		log.Fatalf("failed to open file: %v", err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("scanner error: %v", err)
+	}
+
+	return words
+}
+
+func main() {
+	words := getWordsSlice("input.txt")
+
+	nums := getFirstLastDigit(words)
+	
 	sum := 0
 	for _, num := range nums {
 		sum += num
